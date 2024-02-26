423121c9393494ddfb279c013ee3cccd651ada39
unknown <mukulbhardwaj947@gmail.com>
2024-02-26 11:57:17 +0530

    Solved some more problems

---
+++ towerofhanoi.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void towerofhanoi(int n, int src, int help, int dest)
+{
+    if (n > 0)
+    {
+        towerofhanoi(n - 1, src, dest, help);
+        cout << src << " " << dest << endl;
+        towerofhanoi(n - 1, help, src, dest);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n; // number of disk
+    cout << pow(2, n) - 1 << endl;
+    towerofhanoi(n, 1, 2, 3);
+}
